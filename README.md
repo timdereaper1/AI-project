1cdd7a0e6499110269189cf78c9b116b7e3d8996
timothyKamo <timdereaper1@gmail.com>
2018-10-29 18:25:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Automated Essay Grading
+
+Download the dataset from [here](https://www.kaggle.com/c/asap-aes/data) and place it in a dataset folder
+
+The project is built on python 3, so you have to install python 3. Click [here](https://www.python.org/downloads/release/python-370/) for details on installation
+
+Install the python modules need for the project by running the command
+
+```bash
+pip install -r requirement.txt
+```
+
+After, installing the needed modules, run the command
+
+```bash
+python read_dataset.py
+```
+
+Which creates a data folder of the extracted essays from the excel file placing them in their set folders
+
+---
+
+Read on the information about the dataset [here](https://www.kaggle.com/c/asap-aes/data)
---
+++ utils.py
@@ -0,0 +1,81 @@
+import csv
+import os
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# setting the english dictionary to filter out the stopwords such as
+# off, the, to, etc. from the essays as a set
+stop_words = set(stopwords.words('english'))
+
+
+def get_key(obj):
+    # returns the key of the dictionary
+    return list(obj.keys())[0]
+
+
+def write_csv(filename, matrix, cutoff=2):
+    # Write out the matrix to a csv file. Note that setting cutoff=1 means
+    # that words which appear in 1 or more documents will be included in
+    # the output (i.e. every word will appear in the output). The default
+    # for cutoff is 2, since we usually aren't interested in words which
+    # appear in a single document. For this example we want to see all
+    # words however, hence cutoff=1.
+    # create an instance of the csv object to write the file to
+    file = csv.writer(open(filename, 'w'))
+    # write each row in the matrix to the file
+    for row in matrix.rows(cutoff=cutoff):
+        file.writerow(row)
+
+
+def filter_document(document):
+    # tokenize the essays into words such as ['this', 'as'] etc
+    tokenize_document = word_tokenize(document)
+
+    # remove the stopwords from the essay using the english dictionary version
+    # since the essays were written in english
+    filter_document = [
+        word for word in tokenize_document if word not in stop_words]
+
+    # return the filtered document
+    return filter_document
+
+
+def write_txt(filename, text):
+    # open the file for writing
+    file = open(filename + '.txt', 'w')
+    # write the text to the file
+    file.write(text)
+    file.flush()
+
+
+def get_essay_set(essay_set):
+    # create a list of dictionary with the the essay_set having the key to
+    # by the id of the essay set and the value being the total number of
+    # essays for that set
+    setList = [{i: 0} for i in set(essay_set)]
+
+    # iterating through the setIds find the length or the total number of essays
+    # for the id and set its value with the number
+    for setId in setList:
+        # get the key of the setId such as {1: 0} i.e. key = 1
+        key = get_key(setId)
+        # iterate the essay_set then match the value to the key
+        # if both matches add a value of 1 to the value of the setId
+        # example [1, 1, 1, 2] => [{1: 3}, {2: 1}]
+        for essaySet in essay_set:
+            if essaySet == key:
+                setId[key] = setId[key] + 1
+
+    return setList
+
+
+def create_data_dir(sets, essays, dir_name='/data'):
+    i = 0
+    while i < len(sets):
+        directory = str(dir_name) + str(sets[i])
+        try:
+            os.mkdir(directory)
+        except:
+            pass
+        write_txt(directory + '/' + str(i), essays[i])
+        i = i + 1
