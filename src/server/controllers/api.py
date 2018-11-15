8e35764ff8c706b522d8dcd9b528fcb29d1cff11
Amo Timothy <timdereaper1@gmail.com>
2018-11-15 21:56:05 +0000

    --amend

--- src/server/controllers/api.py
+++ src/server/controllers/api.py
@@ -1,8 +1,9 @@
-from flask import jsonify
+from flask import jsonify, Request
 from flask_restful import Resource
 
 
 class EssayGrader(Resource):
     def post(self):
+        # get the essay from the post request
         # the method marks the essay and returns the results as a json
         return jsonify({'score': 'essay score'})
