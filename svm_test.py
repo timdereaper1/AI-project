734ea9871c59a441955bcc6b4539ae9b44f574b1
Amo Timothy <timdereaper1@gmail.com>
2018-11-27 10:35:32 +0000

    modified grading model to svm

--- svm_test.py
+++ svm_test.py
@@ -4,10 +4,10 @@ from src.utils.extract import Extract
 
 # load the validation set from the dataset valic_set.xls file using
 # pandas
-df = pd.read_excel('dataset/valid_set.xls')
+df = pd.read_excel('dataset/training_set_rel3.xls')
 
 # the essay set to be used to validate the essay
-essays = df['essay']
+essays, scores = df['essay'], df['domain1_score']
 
 # load the marker model
 model = joblib.load('models/marker_svm.joblib')
@@ -16,17 +16,19 @@ model = joblib.load('models/marker_svm.joblib')
 # have been predicted
 index = 0
 
+done = 0
+
 # validate the model with the validation set of essays
-for essay in essays:
+for essay in essays[:10]:
     # extract the features needed by the model from the essay
     features = Extract(essay)
 
     # predict score of the essay
-    score = model.predict([features.get_features()])
-    print(score)
-
-    # predict the score of the first 11 essays
-    if index == 5:
-        break
+    score = model.predict([features.get_features()])[0]
+    if score == scores[index]:
+        done += 1
 
     index += 1
+
+accuracy = done / 10 * 100
+print(accuracy)
