1c4c48c7f53428f3c1287ea29205b817d4035227
Amo Timothy <timdereaper1@gmail.com>
2018-12-04 01:05:01 +0000

    designed base structure interface for dashboard pages

--- src/client/Dashboard/Dashboard.jsx
+++ src/client/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RouteComponentProps, Route, Switch } from 'react-router-dom';
+import { Header } from 'semantic-ui-react';
 import Sidebar from './Sidebar';
 import './css/dashboard.css';
 import { routes } from './routes';
@@ -8,15 +9,21 @@ class Dashboard extends React.Component<RouteComponentProps> {
 	state = {};
 
 	render(): React.ReactNode {
+		const selectedRoute = routes.find(value => value.path === this.props.location.pathname);
 		return (
 			<div className="dashboard">
-				<Sidebar links={routes} selectedPage={this.props.location.pathname} />
-				<Switch>
-					{routes.map((route, _i) => (
-						/* eslint-disable react/no-array-index-key */
-						<Route key={_i} {...route} />
-					))}
-				</Switch>
+				<Sidebar links={routes} selectedPage={selectedRoute.path} />
+				<div className="content-wrapper">
+					<div style={{ width: '100%' }}>
+						<Header className="title">{selectedRoute.name}</Header>
+						<Switch>
+							{routes.map((route, _i) => (
+								/* eslint-disable react/no-array-index-key */
+								<Route key={_i} {...route} />
+							))}
+						</Switch>
+					</div>
+				</div>
 			</div>
 		);
 	}
