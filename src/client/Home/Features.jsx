4cd4bb75bacc21b495ba153334dc6c8e60d36743
Amo Timothy <timdereaper1@gmail.com>
2018-12-12 19:55:40 +0000

    design home page

---
+++ src/client/Home/Features.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Segment, Header, Icon, Grid } from 'semantic-ui-react';
+import './css/home.css';
+
+const Features: React.FunctionComponent<{}> = () => (
+	<Segment className="services" padded>
+		<Grid>
+			<Grid.Row columns="3">
+				<Grid.Column>
+					<Header>Scoring</Header>
+					<Icon name="check" className="service-icon" />
+					<p>Score essays easily and faster with AMA optimized scoring system.</p>
+				</Grid.Column>
+				<Grid.Column>
+					<Header>Grading</Header>
+					<Icon name="file alternate outline" className="service-icon" />
+					<p>AMA grades essays using standand essay grading with set criteria.</p>
+				</Grid.Column>
+				<Grid.Column>
+					<Header>Analysis</Header>
+					<Icon name="area chart" className="service-icon" />
+					<p>Visualize and analyze AMA scores on essays and improve essay writing.</p>
+				</Grid.Column>
+			</Grid.Row>
+		</Grid>
+	</Segment>
+);
+
+export default Features;
--- src/client/Home/Home.jsx
+++ src/client/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Header, Segment, Grid, Icon } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 import { RouteComponentProps, Link } from 'react-router-dom';
 import './css/home.css';
 import image from '../_shared/assets/imgs/firs.jpg';
 import Footer from './Footer';
+import Features from './Features';
 
 interface Styles {
 	[key: string]: React.CSSProperties;
@@ -15,16 +16,19 @@ class Home extends React.Component<RouteComponentProps> {
 		this.bannerRef = React.createRef();
 	}
 
-	componentDidMount() {
-		window.addEventListener('scroll', e => {});
-	}
-
-	componentWillUnmount() {
-		window.removeEventListener('scroll');
+	componentWillMount() {
+		this.event = window.addEventListener(
+			'scroll',
+			() => {
+				if (this.bannerRef.current) {
+					this.bannerRef.current.style.backgroundPositionY = `${-window.scrollY / 3}px`;
+				}
+			},
+			false
+		);
 	}
 
 	render(): React.ReactNode {
-		console.log(this.bannerRef);
 		return (
 			<div className="home">
 				<div className="banner" style={styles.wrapper} ref={this.bannerRef}>
@@ -42,54 +46,14 @@ class Home extends React.Component<RouteComponentProps> {
 						</Link>
 					</div>
 				</div>
-				<Segment className="services" padded>
-					<Grid>
-						<Grid.Row columns="3">
-							<Grid.Column>
-								<Header>Scoring</Header>
-								<Icon name="check" className="service-icon" />
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Exercitationem, minus hic error
-								</p>
-							</Grid.Column>
-							<Grid.Column>
-								<Header>Grading</Header>
-								<Icon name="file alternate outline" className="service-icon" />
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Exercitationem, minus hic error
-								</p>
-							</Grid.Column>
-							<Grid.Column>
-								<Header>Analysis</Header>
-								<Icon name="area chart" className="service-icon" />
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Exercitationem, minus hic error
-								</p>
-							</Grid.Column>
-						</Grid.Row>
-					</Grid>
-				</Segment>
-				<p style={{ padding: '0 1rem' }}>
-					AMA is an AI agent trained to access and score english essays with selected
-					criteria standards for essay scoring. Click here to view criteria. <br />
-					The title of the essay is required and must be entered in the input field below,
-					else AMA cannot fully access the essay. Type in your essay in the provided text
-					editor, then click on submit button to submit the essay for marking.
-				</p>
-				<p className="desc">
-					AMA shows you the results of your essay through charts and graphs. <br />
-					The pie chart shows the standard elements taken from scoring an essay such as
-					number of misspelt words and others <br />
-					The line chart shows the total score given to various parts of speech written in
-					the essay such as nouns, verbs and others. Click on the view button to see all
-					the scores on parts of speech for the essay.
-				</p>
+				<Features />
 				<Footer />
 			</div>
 		);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.event, false);
 	}
 }
 
