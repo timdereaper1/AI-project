c3c9969b1bb0de09585a0c4d164ddba776f48073
Amo Timothy <timdereaper1@gmail.com>
2018-12-15 16:08:27 +0000

    modified svm_train.py to use kFold and error metrics

--- svm_train.py
+++ svm_train.py
@@ -3,6 +3,8 @@ import math
 import pandas as pd
 from sklearn import svm
 from sklearn.externals import joblib
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
 from src.server.utils.extract import Extract
 
 df = pd.read_excel('dataset/training_set_rel3.xls')
@@ -16,15 +18,45 @@ for essay in essays:
     extract = Extract(essay)
     score = scores[counter]
     if math.isinf(score) or math.isnan(score):
-        score = 0
+        score = -9999
 
     data.append(extract.get_features())
     labels.append(score)
 
     counter += 1
 
-X = np.array(data)
-model = svm.SVC(kernel='linear', C=1.0)
-model.fit(X, labels)
+# list for storing model and error pair for Kfold
+models = []
+model_errors = []
 
-joblib.dump(model, 'models/marker_svm.joblib')
+# list of predicted scores from train_index in Kfold
+predicted_scores = []
+
+svm_model = svm.SVC(kernel='linear', C=1.0)
+
+kf = KFold(n_splits=5)
+for train_index, test_index in kf.split(data):
+
+    X = np.array(data[train_index])
+
+    # train the model with the X and Y data by fitting the data into the model using the
+    # fit method provided by the LinearRegression class
+    model = svm_model.fit(X, labels[train_index])
+
+    for features in data[test_index]:
+        predicted_values = model.predict([features])[0]
+        # score actually found in predicted_values
+        predicted_scores.append(predicted_values)
+
+    models.append(model)
+    model_errors.append(mean_squared_error(scores, predicted_scores))
+
+min_error = min(model_errors)
+min_error_index = model_errors.index(min_error)
+
+print(model_errors)
+print(min_error)
+print(models)
+print(models[min_error_index])
+
+# joblib.dump(model, 'models/marker_svm.joblib')
